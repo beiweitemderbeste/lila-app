8c82deecbd85424d9108bf87c55f33db963e949c
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-10-02 02:46:28 +0200

    creating loginform component

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,10 +1,35 @@
+import { useForm } from "react-hook-form";
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <>
-    <h1>hello from loginform</h1>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input
+        defaultValue="username"
+        {...register("username", { required: true })}
+      />
+      {errors.password && <span>This field is required</span>}
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input
+        defaultValue="password"
+        {...register("password", { required: true })}
+      />
+      {/* errors will return when field validation fails  */}
+      {errors.password && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
   );
 };
 
 export default LoginForm;
-
