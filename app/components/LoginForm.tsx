2c04b212daa42bf5e1c90a4e840fcec8052de68c
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-10-02 17:36:43 +0200

    adding react hook form + types

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import LoginFormInput from "../interfaces/LoginFormInput";
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInput>();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <h1>hello from LoginForm</h1>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="username" {...register("username", { required: true })} />
+      {errors.password && <span>This field is required</span>}
+
+      <input defaultValue="password" {...register("password", { required: true })} />
+      {errors.password && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default LoginForm;
---
+++ app/interfaces/LoginFormInput.ts
@@ -0,0 +1,6 @@
+interface LoginFormInput {
+  username: string;
+  password: string;
+}
+
+export default LoginFormInput;
