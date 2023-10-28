6ec31b91a14ee6bdbd356e8b683cc462228cf01f
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-10-28 18:35:24 +0200

    testing if heading gets rendered

--- __tests__/components/LilaHeading.test.tsx
+++ __tests__/components/LilaHeading.test.tsx
@@ -4,7 +4,9 @@ import { render, screen } from "@testing-library/react";
 import LilaHeading from "../../app/components/LilaHeading";
 
 describe("LilaHeading", () => {
-  it("renders without errors", () => {
+  it("renders heading lila", () => {
     render(<LilaHeading />);
+    const headerElement = screen.getByText("lila");
+    expect(headerElement).toBeInTheDocument();
   });
 });
